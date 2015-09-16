a2b237852e7f46c90f33283de4460898271ad871
jgu <jeffwgu@gmail.com>
2015-09-16 15:54:12 -0600

    improve formatting

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -1,9 +1,4 @@
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
-%GRADIENTDESCENT Performs gradient descent to learn theta
-%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by
-%   taking num_iters gradient steps with learning rate alpha
-
-% Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
