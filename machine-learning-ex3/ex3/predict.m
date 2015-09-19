5b009e4f7a859dfd9a5321eec716c3834dc01ec6
jgu <jeffwgu@gmail.com>
2015-09-19 17:35:25 -0600

    delete comments

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -3,9 +3,8 @@ function p = predict(Theta1, Theta2, X)
 %   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
 %   trained weights of a neural network (Theta1, Theta2)
 
-% Useful values
 m = size(X, 1);
-num_labels = size(Theta2, 1);
+
 a1 = [ones(m, 1) X];
 
 z2 = a1 * Theta1';
