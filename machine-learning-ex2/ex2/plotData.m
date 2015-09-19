d8d963ea968244e61dc7f938a46e344aaca29742
jgu <jeffwgu@gmail.com>
2015-09-18 19:01:52 -0600

    finish week 2

--- machine-learning-ex2/ex2/plotData.m
+++ machine-learning-ex2/ex2/plotData.m
@@ -4,7 +4,7 @@ function plotData(X, y)
 %   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
 % Create New Figure
-%figure; hold on;
+figure; hold on;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Plot the positive and negative examples on a
@@ -12,6 +12,11 @@ function plotData(X, y)
 %               examples and 'ko' for the negative examples.
 %
 
+% Find Indices of Positive and Negative Examples
+pos = find(y==1); neg = find(y == 0);
+% Plot Examples
+plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, 'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
 
 
 
@@ -24,6 +29,6 @@ function plotData(X, y)
 
 
 
-%hold off;
+hold off;
 
 end
